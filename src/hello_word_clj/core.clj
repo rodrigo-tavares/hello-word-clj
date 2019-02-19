0bd11db7ea5ece52e5b09fc0f081dc39700d1aee
Rodrigo Tavares <rodrigo.tavares@conectt.com.br>
2019-02-19 15:25:13 -0300

    Commit Inicial

---
+++ src/hello_word_clj/core.clj
@@ -0,0 +1,9 @@
+(ns hello-word-clj.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, I'm Rodrigo Tavares!"))
+
+  
---
+++ test/hello_word_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns hello-word-clj.core-test
+  (:require [clojure.test :refer :all]
+            [hello-word-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
